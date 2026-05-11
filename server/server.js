81c63f5d2f78cf7bfa673061d9d3bc43bbc720c5
Gaurav Kumar <gauravkreth@gmail.com>
2026-05-11 18:57:00 +0530

    register and login done in backend

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Tender Management API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
